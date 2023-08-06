876531ecad3aedf2c880e7b865c7d945e4d661eb
MakArtKar <makoyan2001@gmail.com>
2023-08-06 18:41:49 +0000

    updated database

--- src/database.py
+++ src/database.py
@@ -3,12 +3,33 @@ import pandas as pd
 from langchain.embeddings import SentenceTransformerEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.schema import Document
 from datetime import date
 from dotenv import load_dotenv
 
 load_dotenv()
 
 data_path = os.getenv('DATA_PATH')
+
+def convert_documents(documents_list):
+    result_list = []
+    for doc in documents_list:
+        id_message = doc.page_content.split('id_message: ')[1].split('\n')[0]
+        id_user = doc.page_content.split('id_user: ')[1].split('\n')[0]
+        name = doc.page_content.split('name: ')[1].split('\n')[0]
+        text = doc.page_content.split('text: ')[1]
+
+        metadata = doc.metadata.copy()
+        metadata.update({
+            'id_message': id_message,
+            'id_user': id_user,
+            'name': name
+        })
+
+        new_doc = Document(page_content=text, metadata=metadata)
+
+        result_list.append(new_doc)
+    return result_list
 
 class Database:
     def __init__(self, model_name=None):
@@ -39,4 +60,7 @@ class Database:
                 self.faiss_db = FAISS.load_local(faiss_index_path, self.embeddings)
             except:
                 self.initialize_faiss_db()
-        return self.faiss_db.similarity_search(query, k=10)
+
+        ans = convert_documents(self.faiss_db.similarity_search(query, k=10))
+
+        return ans
